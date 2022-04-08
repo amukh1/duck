7d1bb771fb10c36228070c9efae499fa6f6bbeb8
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-04-08 15:12:46 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let xx = eval(this.responseText)
+        console.log(xx)
+        for(i=0;i<this.responseText.length;i++){
+            var newDiv = document.createElement("div");
+            newDiv.innerHTML = `Username: ${xx[i].UserName} | Score: ${xx[i].Highscore}`;
+            document.getElementById("scores").appendChild(newDiv);
+        }
+    }
+};
+xhttp.open("GET", "https://dis.amukh1.dev/lbhh", true);
+xhttp.send();
+
