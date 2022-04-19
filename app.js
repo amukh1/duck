bad452cd2188d0b6c271d3f4b6c2f87283fdfedc
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-04-19 08:19:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         let xx = eval(this.responseText)
+//         console.log(xx)
+//         for(i=0;i<this.responseText.length;i++){
+//             var newDiv = document.createElement("div");
+//             newDiv.innerHTML = `Username: ${xx[i].UserName} | Score: ${xx[i].Kills} | Rank: #${i + 1}`;
+//             document.getElementById("scores").appendChild(newDiv);
+//         }
+//     }
+// };
+// xhttp.open("GET", "https://api.amukh1.dev/getAll", true);
+// xhttp.send();
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -5,11 +20,10 @@ xhttp.onreadystatechange = function() {
         console.log(xx)
         for(i=0;i<this.responseText.length;i++){
             var newDiv = document.createElement("div");
-            newDiv.innerHTML = `Username: ${xx[i].UserName} | Score: ${xx[i].Kills} | Rank: #${i + 1}`;
+            newDiv.innerHTML = `Username: ${xx[i].UserName} | Score: ${xx[i].Highscore}`;
             document.getElementById("scores").appendChild(newDiv);
         }
     }
 };
-xhttp.open("GET", "https://api.amukh1.dev/getAll", true);
+xhttp.open("GET", "https://dis.amukh1.dev/lbhh", true);
 xhttp.send();
-
