818c282249bdf67ed42abe16a817024b89b17ca0
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-05-18 17:54:34 -0400

    e

--- app.js
+++ app.js
@@ -13,10 +13,26 @@
 // xhttp.open("GET", "https://api.amukh1.dev/getAll", true);
 // xhttp.send();
 
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         let xx = JSON.parse(this.responseText)
+//         console.log(xx)
+//         for(i=0;i<this.responseText.length;i++){
+//             var newDiv = document.createElement("div");
+//             newDiv.innerHTML = `Username: ${xx[i].UserName} | Score: ${xx[i].Highscore} | Rank: ${i + 1}`;
+//             document.getElementById("scores").appendChild(newDiv);
+//         }
+//     }
+// };
+// xhttp.open("GET", "https://dis.amukh1.dev/lbhh", true);
+// xhttp.send();
+
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        let xx = eval(this.responseText)
+        let xx = JSON.parse(this.responseText)
         console.log(xx)
         for(i=0;i<this.responseText.length;i++){
             var newDiv = document.createElement("div");
@@ -25,5 +41,5 @@ xhttp.onreadystatechange = function() {
         }
     }
 };
-xhttp.open("GET", "https://dis.amukh1.dev/lbhh", true);
+xhttp.open("GET", "https://api.amukh1.dev/getScores", true);
 xhttp.send();
